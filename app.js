804cb5730609aab06035b7e83c495628efef0ce0
unknown <cameronwfinney@gmail.com>
2023-01-31 15:42:55 +0000

    adding dark mode button

---
+++ app.js
@@ -0,0 +1,16 @@
+// this one is jut to wait for the page to load
+document.addEventListener("DOMContentLoaded", () => {
+  const themeStylesheet = document.getElementById("theme");
+  const themeToggle = document.getElementById("theme-toggle");
+  themeToggle.addEventListener("click", () => {
+    // if it's light -> go dark
+    if (themeStylesheet.href.includes("light")) {
+      themeStylesheet.href = "css/dark-theme.css";
+      themeToggle.innerText = "Switch to light mode";
+    } else {
+      // if it's dark -> go light
+      themeStylesheet.href = "css/light-theme.css";
+      themeToggle.innerText = "Switch to dark mode";
+    }
+  });
+});
